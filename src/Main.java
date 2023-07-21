954ad82e9fb961c1f898be073f91011d3acca86e
BaikalAsanov <asanova.gtbaikal@gmail.com>
2023-07-21 19:26:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animals={new Shark(), new Turtle(), new Eagle()};
+        for (Animal animal: animals) {
+            if (animal instanceof Shark) {
+                ((Shark) animal).attack();
+            } else if (animal instanceof Turtle) {
+                ((Turtle) animal).swim();
+            } else if (animal instanceof Eagle) {
+                ((Eagle) animal).fly();
+            }
+        }
+    }
+}
